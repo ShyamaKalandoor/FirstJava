9a7de65afe28b93ed4f51d663a8283d83145a0d1
Shyama-Home <Shyama-Home@DESKTOP-56MGAH0>
2021-01-30 12:28:42 -0800

    firstjava commit

---
+++ test2/src/test2/testdemo.java
@@ -0,0 +1,21 @@
+package test2;
+
+public class testdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+			        
+			      int num1 = 5, num2 = 15, sum;
+			      sum = num1 + num2;
+
+			      System.out.println("Sum of these numbers: "+sum);
+			   }
+			
+
+	}
+
+
